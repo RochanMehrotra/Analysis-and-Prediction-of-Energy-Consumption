33cf753db9b19ae1db9a0aac0190dd686617fd8b
RochanMehrotra <rochanmehrotra@gmail.com>
2018-11-30 19:34:56 +0530

    -

--- scala/Context.scala
+++
@@ -1,9 +0,0 @@
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
-
-trait Context {
-  val conf = new SparkConf().setMaster("local[4]").setAppName("CdacProject")
-  val spark = SparkSession.builder.config(conf).getOrCreate()
-  val sc=spark.sparkContext
-  val SQLContext=spark.sqlContext
-}
